d8cbe7420125437c9fe47a000db02ba9742433ef
JuusoK <JuusoK@bitbucket.org>
2018-12-13 19:15:28 +0800

    finish addNewProject

---
+++ js/http.js
@@ -0,0 +1,29 @@
+function getCookie(c_name)
+{
+    if (document.cookie.length>0)
+    {
+        c_start=document.cookie.indexOf(c_name + "=")
+        if (c_start!=-1)
+        { 
+            c_start=c_start + c_name.length+1 
+            c_end=document.cookie.indexOf(";",c_start)
+            if (c_end==-1) c_end=document.cookie.length
+            return unescape(document.cookie.substring(c_start,c_end))
+        } 
+    }
+    return ""
+}
+
+function getPar(par){
+    var local_url = document.location.href; 
+    var get = local_url.indexOf(par +"=");
+    if(get == -1){
+        return false;   
+    }   
+    var get_par = local_url.slice(par.length + get + 1);    
+    var nextPar = get_par.indexOf("&");
+    if(nextPar != -1){
+        get_par = get_par.slice(0, nextPar);
+    }
+    return get_par;
+}
